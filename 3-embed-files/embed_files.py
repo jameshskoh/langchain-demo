8de6e1dbe0658d748da916b6f742f3311d5efbee
jameshskoh <huisoonkoh@gmail.com>
2025-08-30 21:00:35 +0800

    Move qwen client to a higher folder; add demo to import, split, and embed a document, and then finally execute query on the document

---
+++ 3-embed-files/embed_files.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from qwen.embedding_client import QwenEmbeddings
+
+
+def split_text(documents: list[Document]) -> list[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=80, add_start_index=True
+    )
+    return text_splitter.split_documents(documents)
+
+
+if __name__ == "__main__":
+    file_path = "../resources/nke-10k-2023.pdf"
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    texts = split_text(docs)
+
+    embeddings = QwenEmbeddings(base_url="http://localhost:1234/v1")
+
+    vector_store = InMemoryVectorStore(embeddings)
+    ids = vector_store.add_documents(documents=texts)
+
+    results = vector_store.similarity_search("How many distribution centers does Nike have in the US?")
+
+    print(results)
+
+    diverse_result = vector_store.max_marginal_relevance_search("How many distribution centers does Nike have in the US?")
+
+    print(diverse_result)
